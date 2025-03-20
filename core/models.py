67fc166a80fd16ed0f4af375bbcc2696c2ffa1d8
Hollman Rojas <hollmanerojas@gmail.com>
2025-03-20 00:16:52 -0500

    Feature/add models of property

--- core/models.py
+++ core/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+
+OWNER_TYPES =(
+        ('Inmobiliaria', 'Inmobiliaria'),
+        ('Particular', 'Particular')
+    )
+
+
+PROPERTY_TYPES =(
+        ('Casa', 'Casa'),
+        ('Apartamento', 'Apartamento'),
+        ('Local', 'Local'),
+        ('Oficina', 'Oficina'),
+        ('Bodega', 'Bodega')
+)
+
+TRANSACTION_TYPES =(
+        ('Venta', 'Venta'),
+        ('Arriendo', 'Arriendo')
+)
+
+
+
 class Location(models.Model):
     department = models.Charfield(max_Length=100)
     city = models.CharField(max_length=100)
@@ -11,3 +33,57 @@ class Location(models.Model):
         
     def __str__(self):
         return f'{self.department},{self.city},{self.district}'
+
+
+class OwnerType(models.Model):
+    name = models.CharField(max_length= 100, unique=True, choices=OWNER_TYPES)
+    
+    def __srt__(self):
+        return self.name
+
+class Owner(models.Model):
+    name = models.CharsField(max_lenght=255)
+    owener_type = models.ForeignKey(OwnerType, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class PropertyType(models.Model):
+    name = models.CharField(max_length=100, unique=True, choices=PROPERTY_TYPES)
+    
+    def __str__(self):
+        return self.name
+
+class TransactionType(models.Model):
+    name = models.CharFiled(max_length=100, unique=True, choices=TRANSACTION_TYPES)
+    
+    def __str__(self):
+        return self.name
+    
+class Property(models.Model):
+    property_type = models.ForeignKey(PropertyType, on_delete=models.CASCADE)
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    onwer = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    stratum = models.IntegerField(null=True, blank=True)
+    floor = models.IntegerField(null=True, blank=True)
+    rooms = models.IntegerField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    publication_date = models.DateField()
+    publication_year = models.IntegerField()
+    publication_month = models.CharField(max_length=20)
+    price_by_m2 =models.DecimalField(max_digits=15, decimal_places=2)
+    total_price = models.DecimalField(max_digits=15, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+    
+    
+    
